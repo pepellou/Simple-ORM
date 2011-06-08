b1cb1c5f3635639ad3b7c162a782440bcd00f7a5
Pablo <pvicente@agil-az.com>
2011-06-08 21:41:06 +0200

    Create valid connection

--- test/DBConnectorTest.php
+++ test/DBConnectorTest.php
@@ -3,14 +3,48 @@
 
 class DBConnectorTest extends PHPUnit_Framework_TestCase {
 
+	private function getValidConnector(){
+		$validHost = '192.168.0.49';
+		$validUser = 'facturador';
+		$validPass = '76900831.d';
+		$validDB = 'FacturAZ';
+		return new DBConnector($validHost, $validUser, $validPass, $validDB);	
+	}
+
 	/**
 	* @expectedException CannotConnectException
 	*/
-	public function test_cannotCreateInvalidConnection(
+	public function test_cannotCreateInvalidConnector(
 	) {
+		$invalidHost = 'invalidHost';
 		$invalidUser = '';
 		$invalidPass = '';
-		new DBConnector($invalidUser,$invalidPass);	
+		$invalidDB = '';
+		new DBConnector($invalidHost, $invalidUser, $invalidPass, $invalidDB);	
 	}
+
+	/**
+	* @expectedException NoSuchDatabaseException
+	*/
+	public function test_cannotCreateWithInvalidDB(
+	) {
+		$validHost = '192.168.0.49';
+		$validUser = 'facturador';
+		$validPass = '76900831.d';
+		$invalidDB = '';
+		new DBConnector($validHost, $validUser, $validPass, $invalidDB);	
+	}
+
+	public function test_canCreateValidConnector(
+	) {
+		$this->getValidConnector();
+	}
+
+	public function test_connectionIsClosedAfterCreating(
+	) {
+		$connector = $this->getValidConnector();
+		$this->assertFalse($connector->hasOpenConnection());
+	}
+
 } 
 ?>
